ab92917094bf554cb2394d18112cb40a880b193e
Hinton <hinton@users.noreply.github.com>
2026-03-16 20:39:44 +0100

    Set site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "static",
+  site: "https://brutnaloften.github.io/hemsida/",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
